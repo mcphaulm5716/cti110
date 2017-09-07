c750565281ea0b2e660f22c2a04e17f9b70bdd11
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-09-07 18:28:20 -0400

    Add files via upload
    First submission.

---
+++ M2T1_SalesPrediction_McPhaul.py
@@ -0,0 +1,11 @@
+#CTI-110
+#M2T1 Sales Prediction
+#Maaka McPhaul
+#9/5/17
+
+#Get the projected total sales.
+total_sales= float (input('Enter the projected sales: '))
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+#Display the profit.
+print ('The profit is $', format (profit, ',.2f'))
