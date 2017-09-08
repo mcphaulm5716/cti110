54c178fd60d3956f26b13fb3e60a3b696ddeda38
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-09-08 12:36:45 -0400

    Add files via upload
    Second submission

---
+++ M2HW1_DistanceTraveled_McPhaul.py
@@ -0,0 +1,81 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #CTI-110
+>>> #M2HW1-Distance Traveled
+>>> #Maaka McPhaul
+>>> #9/8/17
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 1, in <module>
+    distance = speed * time
+NameError: name 'speed' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 1, in <module>
+    distance = 70 * time
+NameError: name 'time' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 3, in <module>
+    print (distanceAfter6)
+NameError: name 'distanceAfter6' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+420
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 4, in <module>
+    print (distanceAfter10)
+NameError: name 'distanceAfter10' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+1050
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 6, in <module>
+    print (distanceAfter10)
+NameError: name 'distanceAfter10' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+420
+700
+1050
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+In 6 hours the distance will be distanceAfter6 miles
+700
+1050
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+420
+700
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 6, in <module>
+    print (distanceAfter10)+ (miles)
+NameError: name 'miles' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 5, in <module>
+    print ('In 6 hours the car will travel' + (distanceAfter6) + 'miles')
+TypeError: must be str, not int
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+In 6 hours the car will travel420miles
+700
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py", line 6, in <module>
+    print (distanceAfter10)+ (miles)
+NameError: name 'miles' is not defined
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+In 6 hours the car will travel420miles
+In 10 hours the car will travel700miles
+In 15 hours the car will travel1050miles
+>>> 
+======== RESTART: C:/Users/McPhaul/Desktop/M2HW1_DistanceTraveled_.py ========
+In 6 hours the car will travel420miles
+In 10 hours the car will travel700miles
+In 15 hours the car will travel1050miles
+>>> 
