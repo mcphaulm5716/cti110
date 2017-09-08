1b71cc4bee77718881b29aca2be23499509c7391
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-09-08 14:05:40 -0400

    Add files via upload
    Third submission.

---
+++ M2HW2_TipTaxTotal_McPhaul.py
@@ -0,0 +1,86 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:14:34) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> #CTI-110
+>>> #M2HW2-Tip Tax Total
+>>> #Maaka McPhaul
+>>> #9/8/17
+>>> 
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal330
+foodCost $.2f
+tip $.2f
+salesTax $.2f
+total $.2f
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal 450
+foodCost $450.00
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py", line 7, in <module>
+    print ('tip $' + format ( tip, ',.2f' ))
+NameError: name 'tip' is not defined
+>>> 
+>>> 
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal450
+foodCost $450.00
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py", line 7, in <module>
+    print ('tip $' + format ( tip, ',.2f' ))
+NameError: name 'tip' is not defined
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal100
+foodCost $100.00
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py", line 7, in <module>
+    print ('tip $' + format( tip, ',.2f' ))
+NameError: name 'tip' is not defined
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal100
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py", line 6, in <module>
+    print ('foodCost $' + ( foodCost, ',.2f' ))
+TypeError: must be str, not tuple
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal100
+foodCost $100.00
+tipAmount $18.00
+salesTax $7.00
+Traceback (most recent call last):
+  File "C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py", line 9, in <module>
+    print ('total $' + format( total, ',.2f'))
+NameError: name 'total' is not defined
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal100
+foodCost $100.00
+tipAmount $18.00
+salesTax $7.00
+totalCost $100.25
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal 230
+foodCost $230.00
+tipAmount $41.40
+salesTax $16.10
+totalCost $230.25
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal 200
+foodCost $200.00
+tipAmount $36.00
+salesTax $14.00
+totalCost $250.00
+>>> 
+=========== RESTART: C:/Users/McPhaul/Desktop/M2HW2_TipTaxTotal.py ===========
+enter cost of the meal 100
+foodCost $100.00
+tipAmount $18.00
+salesTax $7.00
+totalCost $125.00
+>>> 
