6d08f2ca560fb8a156eeb78fbabb17f3cd58677a
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-09-14 18:36:52 -0400

    Add files via upload
    Debugging grades.

---
+++ M3LAB_McPhaul.py
@@ -0,0 +1,35 @@
+#CTI-110
+#M3LAB: Debugging
+#Maaka McPhaul
+#9/14/17
+
+
+def main() :
+    #This program takes a number grade and outputs a letter grade.
+
+    #system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    F_score = 50
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    else:
+        if score > B_score:
+            print('Your grade is: B')
+        else:
+            if score > C_score:
+                print('Your grade is: C')
+            else:
+                if score > D_score:
+                    print('Your grade is: D')
+                else:
+                    if score > F_score:
+                        print('Your grade is: F')
+                    
+
+main()    
