913788b1dce77a8b831e7a70b0ae6d72ee574cac
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-09-19 21:33:56 -0400

    Add files via upload
    SoftwareSales and Discounts

---
+++ M3HW1_SoftwareSales_McPhaul.py
@@ -0,0 +1,23 @@
+#CTI-110
+#M3HW1 Software Sales
+#Maaka McPhaul
+#9/19/17
+
+
+    
+userNumberOfPackages = int( input('Please enter the number of packages purchased: ' ))
+packagePrice = 99
+
+if userNumberOfPackages < 10: discount = 0;
+elif userNumberOfPackages < 20: discount = 0.10
+elif userNumberOfPackages < 50: discount = 0.20
+elif userNumberOfPackages < 100: discount = 0.30
+else: discount = 0.40
+
+subTotal = userNumberOfPackages * packagePrice
+discountAmount = discount * subTotal
+total = subTotal - discountAmount
+
+print("\nAmount of discount: $" + format( discountAmount, ',.2f' ) + \
+      "\nTotal amount: $" + format( total, ",.2f" ))
+                            
