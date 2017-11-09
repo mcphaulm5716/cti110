3a988070191ba29287430e9c95f3818311e5c29c
mcphaulm5716 <31747967+mcphaulm5716@users.noreply.github.com>
2017-11-09 02:01:07 -0500

    Add files via upload
    runningTotal

---
+++ m5hw2_RunningTotal_McPhaul.py
@@ -0,0 +1,11 @@
+#M5HW2 Running Total
+#CTI-110
+#Maaka McPhaul
+#10/13/17
+
+total=0
+while total != "":
+    newNumber = int (input("Enter a number:"))
+    total += newNumber
+print("The total is: ",)
+   
